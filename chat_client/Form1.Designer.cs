7c0da9b3d61d96f7eea98ee10f2a04e3935339e4
Missy Bowles <hbowles@accruent.com>
2015-12-18 13:15:20 -0600

    Adding existing stuff to the repo

---
+++ chat_client/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace chat_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIP = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFriendPort = new System.Windows.Forms.TextBox();
+            this.textFriendIP = new System.Windows.Forms.TextBox();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Messages = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new chat_client.ImprovedListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textLocalPort);
+            this.groupBox1.Controls.Add(this.textLocalIP);
+            this.groupBox1.Location = new System.Drawing.Point(30, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 137);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ip Address";
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.Location = new System.Drawing.Point(92, 92);
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(167, 20);
+            this.textLocalPort.TabIndex = 1;
+            // 
+            // textLocalIP
+            // 
+            this.textLocalIP.Location = new System.Drawing.Point(92, 48);
+            this.textLocalIP.Name = "textLocalIP";
+            this.textLocalIP.Size = new System.Drawing.Size(167, 20);
+            this.textLocalIP.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textFriendPort);
+            this.groupBox2.Controls.Add(this.textFriendIP);
+            this.groupBox2.Location = new System.Drawing.Point(408, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(295, 137);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ip Address";
+            // 
+            // textFriendPort
+            // 
+            this.textFriendPort.Location = new System.Drawing.Point(113, 92);
+            this.textFriendPort.Name = "textFriendPort";
+            this.textFriendPort.Size = new System.Drawing.Size(167, 20);
+            this.textFriendPort.TabIndex = 2;
+            // 
+            // textFriendIP
+            // 
+            this.textFriendIP.Location = new System.Drawing.Point(113, 48);
+            this.textFriendIP.Name = "textFriendIP";
+            this.textFriendIP.Size = new System.Drawing.Size(167, 20);
+            this.textFriendIP.TabIndex = 1;
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(30, 451);
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(673, 20);
+            this.textMessage.TabIndex = 3;
+            this.textMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textMessage_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(709, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(715, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.AutoArrange = false;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Messages});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(30, 222);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(673, 223);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Messages
+            // 
+            this.Messages.Text = "";
+            this.Messages.Width = 669;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(52, 175);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::chat_client.Properties.Resources.greenlight;
+            this.button3.Location = new System.Drawing.Point(709, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 57);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox1.Location = new System.Drawing.Point(210, 213);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(493, 232);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(30, 229);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(673, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 490);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textFriendPort;
+        private System.Windows.Forms.TextBox textFriendIP;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Messages;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private ImprovedListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ chat_client/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net.NetworkInformation;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Win32;
+
+
+namespace chat_client
+{
+    public partial class Form1 : Form
+    {
+        private Socket sck;
+        private EndPoint epLocal, epRemote;
+        private string lHostName, oHostName, lHostChat, oHostChat;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           SystemEvents.SessionSwitch +=
+                         new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            textLocalIP.Text = GetLocalIP();
+            pictureBox1.Image = Properties.Resources.whitelight;
+            label5.Text = "Offline";
+
+            lHostName = Dns.GetHostName();
+            
+            if (lHostName == "JSANCHEZ-LT")
+            {
+                groupBox1.Text = "Jesse";
+                groupBox2.Text = "Missy";
+                oHostName = "HBOWLES-LT";
+                oHostChat = "Missy";
+                lHostChat = "Jesse";
+            }
+            else if (lHostName == "HBOWLES-LT")
+            {
+                groupBox1.Text = "Missy";
+                groupBox2.Text = "Jesse";
+                oHostName = "JSANCHEZ-LT";
+                oHostChat = "Jesse";
+                lHostChat = "Missy";
+            }
+
+            try
+            {
+                textFriendIP.Text = GetRemoteIPByName(oHostName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                textFriendIP.Text = "127.0.0.1";
+            }
+            textLocalPort.Text = "80";
+            textFriendPort.Text = "80";
+            button1.Focus();
+            button2.Enabled = false;
+            //listView1.Visible = false;
+
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            string ipAddress = "127.0.0.1";
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return ipAddress;
+        }
+
+        private string GetRemoteIPByName(string rHost)
+        {
+            IPHostEntry hostMissy = Dns.GetHostByName(rHost);
+            string ipAddress = "127.0.0.1";
+            foreach (IPAddress ip in hostMissy.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    pictureBox1.Image = Properties.Resources.greenlight2;
+                    label5.Text = "Online";
+                    return ip.ToString();
+                }
+            }
+            return ipAddress;
+
+        }
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+                    receivedData = (byte[]) aResult.AsyncState;
+
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receivedMessage = eEncoding.GetString(receivedData);
+
+
+                    if (AnalyzeReceived(receivedMessage.Trim('\0')) == 1) //1 will be acutal message
+                    {
+
+                        string message = oHostChat + ": " + receivedMessage;
+
+                        //ListViewItem lvi = new ListViewItem(message);
+                        //lvi.ForeColor = Color.Red;
+                        //listView1.Items.Add(lvi);
+                        //listView1.Items[listView1.Items.Count - 1].EnsureVisible();
+
+                        //richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        //richTextBox1.ScrollToCaret();
+                        //Clipboard.SetText(message);
+                        //richTextBox1.Paste();
+                        //richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        //richTextBox1.ScrollToCaret();
+                        //richTextBox1.Find(message);
+                        //richTextBox1.SelectionColor = Color.Red;
+                        AppendText(message, Color.Red, true);
+                        AppendText("", Color.Azure, true);
+                        ReceiveSound();
+                        FlashWindow.Flash(this);
+
+                        //listBox1.Items.Add(oHostChat + ": " + receivedMessage);
+                        //((ImprovedListBox) listBox1).SetItemColor(listBox1.Items.Count - 1, System.Drawing.Color.Red);
+                        //listBox1.TopIndex = listBox1.Items.Count - 1;
+                        //ReceiveSound();
+                        //FlashWindow.Flash(this);
+                    }
+                }
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote,
+                    new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                epLocal = new IPEndPoint(IPAddress.Parse(textLocalIP.Text), Convert.ToInt32(textLocalPort.Text));
+                sck.Bind(epLocal);
+
+                epRemote = new IPEndPoint(IPAddress.Parse(textFriendIP.Text), Convert.ToInt32(textFriendPort.Text));
+                sck.Connect(epRemote);
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote,
+                    new AsyncCallback(MessageCallBack), buffer);
+
+                button1.Text = "Connected";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                textMessage.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+               byte[] msg = new byte[1500];
+                msg = enc.GetBytes(textMessage.Text);
+
+                sck.Send(msg);
+
+                int size = msg.Length;
+                
+                if (size > 0)
+                {
+                    string message = "Me: " + textMessage.Text;
+                    
+                    
+                    AppendText(message, Color.Blue, true);
+                    
+                    
+                     //ListViewItem lvi = new ListViewItem(message);
+                    //lvi.ForeColor = Color.Blue;
+                    //listView1.Items.Add(lvi);
+                    //listView1.Items[listView1.Items.Count - 1].EnsureVisible();
+                    SendSound();
+
+                    //richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    //richTextBox1.ScrollToCaret();
+                    //Clipboard.SetText(message);
+                    //richTextBox1.Paste();
+                    //richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    //richTextBox1.ScrollToCaret();
+                    //richTextBox1.Find(message);
+                    //richTextBox1.SelectionColor = Color.Blue;
+
+                    //listBox1.Items.Add("Me: " + textMessage.Text);
+                    //((ImprovedListBox)listBox1).SetItemColor(listBox1.Items.Count-1, System.Drawing.Color.Blue);
+                    //listBox1.TopIndex = listBox1.Items.Count-1;
+                    //SendSound();
+                }
+               
+                textMessage.Clear();
+           }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void textMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button2.PerformClick();
+                // these last two lines will stop the beep sound
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+
+        private void ReceiveSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.boing_spring);
+            simpleSound.Play();
+        }
+
+        private void SendSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.bloop_x);
+            simpleSound.Play();
+        }
+
+        void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            byte[] msg = new byte[1500];
+            switch (e.Reason)
+            {
+                // ...
+                case SessionSwitchReason.SessionLock:
+                    // Do whatever you need to do for a lock
+                    msg = enc.GetBytes("***###LOCK###***");
+                    sck.Send(msg);
+                    break;
+                case SessionSwitchReason.SessionUnlock:
+                    // Do whatever you need to do for an unlock
+                    msg = enc.GetBytes("***###UNLOCK###***");
+                    sck.Send(msg);
+                    break;
+                case SessionSwitchReason.ConsoleDisconnect:
+                    msg = enc.GetBytes("***###OFF###***");
+                    sck.Send(msg);
+                    break;
+                    // ...
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+           // ImageList imgs = new ImageList();
+           // imgs.ImageSize = new Size(50, 50);
+           // imgs.Images.Add("Green", Properties.Resources.greenlight);
+
+           //// listView1.View = View.LargeIcon;
+           // listView1.LargeImageList = imgs;
+
+           // ListViewItem item = new ListViewItem();
+           // item.ImageIndex = 0;
+           // listView1.Items.Add(item);
+
+
+            Image img = Properties.Resources.greenlight;
+            AppendText("Me :", Color.Blue, true);
+
+            Clipboard.SetImage(img);
+                                   
+            Clipboard.Clear();
+       
+            AppendText("", Color.Blue, true);
+            
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            byte[] msg = new byte[1500];
+            msg = enc.GetBytes("***###IMG_GREEN###***");
+            sck.Send(msg);
+           
+           //ListViewItem item = new ListViewItem();
+            //listView1.Items.Add(Image.FromFile(Properties.Resources.greenlight));
+            //item.ImageIndex = Properties.Resources.greenlight;
+            // listView1.Items.Add(item);
+        }
+
+        public void AppendText(string text, Color color, bool AddNewLine = false)
+        {
+            if (AddNewLine)
+            {
+                text += Environment.NewLine;
+            }
+
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.SelectionLength = 0;
+
+            richTextBox1.SelectionColor = color;
+            richTextBox1.AppendText(text);
+            richTextBox1.SelectionColor = richTextBox1.ForeColor;
+            richTextBox1.ScrollToCaret();
+        }
+
+        public int AnalyzeReceived(string transmission)
+        {
+            switch (transmission)
+            {    case "***###LOCK###***": 
+                    pictureBox1.Image = Properties.Resources.yellowlight;
+                    label5.Text = "Away";
+                    return 0;
+                    break;
+                case "***###UNLOCK###***":
+                    pictureBox1.Image = Properties.Resources.greenlight2;
+                    label5.Text = "Online";
+                    return 0;
+                    break;
+                case "***###IMG_GREEN###***":
+                    Image img = Properties.Resources.greenlight;
+                    AppendText("Me :", Color.Blue, true);
+            
+                    Clipboard.SetImage(img);
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToCaret();
+                    richTextBox1.Paste();
+                    Clipboard.Clear();
+                    AppendText("", Color.Blue, true);
+                    return 0;
+                    break;
+               default:
+                    return 1;
+            }
+                  
+        }
+    }
+}
